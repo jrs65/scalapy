85f0cf76cf7247131a8e32f6c028f29ebd86a632
Richard Shaw <jrs65@cita.utoronto.ca>
2011-06-02 12:16:22 -0400

    * Documentation.

--- scroutine.pyx
+++ scroutine.pyx
@@ -16,8 +16,31 @@ cdef int _ONE = 1
 cdef int _ZERO = 0
 
 
-def pdsyevd(mat, destroy = True):
+def pdsyevd(mat, destroy = True, upper = True):
+    r"""Compute the eigen-decomposition of a symmetric matrix.
 
+    Use Scalapack to compute the eigenvalues and eigenvectors of a
+    distributed matrix.
+
+    Parameters
+    ----------
+    mat : DistributedMatrix
+        The matrix to decompose.
+    destroy : boolean, optional
+        By default the input matrix is destroyed, if set to False a
+        copy is taken and operated on.
+    upper : boolean, optional
+        Scalapack uses only half of the matrix, by default the upper
+        triangle will be used. Set to False to use the lower triangle.
+
+    Returns
+    -------
+    evals : np.ndarray
+        The eigenvalues of the matrix, they are returned as a global
+        numpy array of all values.
+    evecs : DistributedMatrix
+        The eigenvectors as a DistributedMatrix.
+    """
     cdef int lwork, liwork
     cdef double * work
     cdef double wl
@@ -35,9 +58,11 @@ def pdsyevd(mat, destroy = True):
     liwork = 7*A.Nr + 8*A.context.num_cols + 2
     iwork = <int *>malloc(sizeof(int) * liwork)
 
+    uplo = "U" if upper else "L"
+
     ## Workspace size inquiry
     lwork = -1
-    pdsyevd_("V", "U", &(A.Nr),
+    pdsyevd_("V", uplo, &(A.Nr),
              A._data(), &_ONE, &_ONE, A._getdesc(),
              <double *>np_data(evals),
              evecs._data(), &_ONE, &_ONE, evecs._getdesc(),
@@ -49,7 +74,7 @@ def pdsyevd(mat, destroy = True):
     work = <double *>malloc(sizeof(double) * lwork)
 
     ## Compute eigen problem
-    pdsyevd_("V", "U", &(A.Nr),
+    pdsyevd_("V", uplo, &(A.Nr),
              A._data(), &_ONE, &_ONE, A._getdesc(),
              <double *>np_data(evals),
              evecs._data(), &_ONE, &_ONE, evecs._getdesc(),
