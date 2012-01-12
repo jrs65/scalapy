4e64c52e38d9a6190d20fc3f8e7e01f1cf817bad
Richard Shaw <jrs65@cita.utoronto.ca>
2012-01-12 13:00:41 -0500

    * MPI-IO write routine. * Added displacement option.

--- pyscalapack/blockcyclic.py
+++ pyscalapack/blockcyclic.py
@@ -2,6 +2,13 @@ import numpy as np
 
 from mpi4py import MPI
 
+
+# Map numpy type into MPI type
+_typemap = { np.float32 : MPI.FLOAT,
+            np.float64 : MPI.DOUBLE,
+            np.complex128 : MPI.COMPLEX16 }
+
+
 def ceildiv(x, y):
     """Round to ceiling division."""
     return ((int(x) - 1) / int(y) + 1)
@@ -91,17 +98,44 @@ def indices_rc(N, B, p, P):
 
 
 
-def mpi_readmatrix(fname, comm, gshape, dtype, blocksize, process_grid, order='F'):
 
-    # Map numpy type into MPI type
-    typemap = { np.float32 : MPI.FLOAT,
-                np.float64 : MPI.DOUBLE,
-                np.complex128 : MPI.COMPLEX16 }
 
-    if dtype not in typemap:
+def mpi_readmatrix(fname, comm, gshape, dtype, blocksize, process_grid, order='F', displacement=0):
+    """Distribute a block cyclic matrix read from a file.
+    
+    Uses MPI-IO to do all the work.
+    
+    Parameters
+    ----------
+    fname : string
+        Name of file to read.
+    comm : mpi4py.MPI.COMM
+        MPI communicator to use. Must match with the one used by BLACS (if using
+        Scalapack).
+    gshape : (nrows, ncols)
+        Shape of the global matrix.
+    blocksize : (blockrows, blockcols)
+        Blocking size for distribution.
+    process_grid : (prows, pcols)
+        The shape of the process grid. Must be the same total size as
+        comm.Get_rank(), and match the BLACS grid (if using Scalapack).
+    order : 'F' or 'C', optional
+        Is the matrix on disk in Fortran (column major) 'F', or C (row major)
+        'C'. Defaults to Fortran ordered.
+    displacement : integer, optional
+        Use a displacement from the start of the file. That is ignore the first
+        `displacement` bytes.
+
+    Returns
+    -------
+    local_array : np.ndarray
+        The section of the array local to this process.
+    """
+    if dtype not in _typemap:
         raise Exception("Unsupported type.")
 
-    mpitype = typemap[dtype]
+    # Get MPI type
+    mpitype = _typemap[dtype]
 
 
     # Sort out F, C ordering
@@ -134,7 +168,6 @@ def mpi_readmatrix(fname, comm, gshape, dtype, blocksize, process_grid, order='F
 
     # Check to see if they type has the same shape.
     if lshape[0]*lshape[1] != darr.Get_size() / mpitype.Get_size():
-        print lshape[0]*lshape[1], darr.Get_size(), mpitype.Get_size(), process_position
         raise Exception("Strange mismatch is local shape size.")
 
 
@@ -143,13 +176,97 @@ def mpi_readmatrix(fname, comm, gshape, dtype, blocksize, process_grid, order='F
 
     # Open the file, and read out the segments
     f = MPI.File.Open(comm, fname, MPI.MODE_RDONLY)
-    f.Set_view(0, mpitype, darr, "native")
+    f.Set_view(displacement, mpitype, darr, "native")
     f.Read_all(local_array)
     f.Close()
 
     return local_array
 
+
+
     
+def mpi_writematrix(fname, local_array, comm, gshape, dtype,
+                    blocksize, process_grid, order='F', displacement=0):
+    
+    """Write a block cyclic distributed matrix to a file.
+    
+    Uses MPI-IO to do all the work.
+    
+    Parameters
+    ----------
+    fname : string
+        Name of file to read.
+    local_array : np.ndarray
+        The array to write.
+    comm : mpi4py.MPI.COMM
+        MPI communicator to use. Must match with the one used by BLACS (if using
+        Scalapack).
+    gshape : (nrows, ncols)
+        Shape of the global matrix.
+    blocksize : (blockrows, blockcols)
+        Blocking size for distribution.
+    process_grid : (prows, pcols)
+        The shape of the process grid. Must be the same total size as
+        comm.Get_rank(), and match the BLACS grid (if using Scalapack).
+    order : 'F' or 'C', optional
+        Is the matrix on disk in Fortran (column major) 'F', or C (row major)
+        'C'. Defaults to Fortran ordered.
+    displacement : integer, optional
+        Use a displacement from the start of the file. That is ignore the first
+        `displacement` bytes.
+
+    """
+    
+    if dtype not in _typemap:
+        raise Exception("Unsupported type.")
+
+    # Get MPI type
+    mpitype = _typemap[dtype]
+
+
+    # Sort out F, C ordering
+    if order not in ['F', 'C']:
+        raise Exception("Order must be 'F' (Fortran) or 'C'")
+
+    mpiorder = MPI.ORDER_FORTRAN # Check what we need to do here for loading C
+                                 # ordered files.
+
+
+    # Get MPI process info
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    # Check process grid shape
+    if size != process_grid[0]*process_grid[1]:
+        raise Exception("MPI size does not match process grid.")
+
+
+    # Create distributed array view.
+    darr = mpitype.Create_darray(size, rank, gshape,
+                                 [MPI.DISTRIBUTE_CYCLIC, MPI.DISTRIBUTE_CYCLIC],
+                                 blocksize, process_grid, mpiorder)
+    darr.Commit()
+
+    # Check to see if they type has the same shape.
+    if local_array.size != darr.Get_size() / mpitype.Get_size():
+        raise Exception("Local array size is not consistent with array description.")
+
+    # Length of filename required for write (in bytes).
+    filelength = displacement + darr.Get_size()
+
+    # Open the file, and read out the segments
+    f = MPI.File.Open(comm, fname, MPI.MODE_RDWR | MPI.MODE_CREATE)
+
+    # Preallocate to ensure file is long enough for writing.
+    f.Preallocate(filelength)
+    
+    f.Set_view(displacement, mpitype, darr, "native")
+    f.Write_all(local_array)
+    f.Close()
+
+    
+
+
 
 def bc_matrix_forward(src, blocksize, process, process_grid, order='F', dest=None):
 
